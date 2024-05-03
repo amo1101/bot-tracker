e4f08dcb19fd505b98ae0b4dd80df877ac0b325d
chumao wu <chumao1101@gmail.com>
2024-05-03 18:48:41 +1200

    add stop_virt.sh

--- test/stop_virt.sh
+++ test/stop_virt.sh
@@ -8,36 +8,36 @@ sudo systemctl disable libvirtd{,-ro,-admin,-tcp,-tls}.socket
 #enable qemu related modular services
 for drv in qemu interface network nodedev nwfilter secret storage 
 do
-  sudo systemctl unmask virt${drv}d.service
-  sudo systemctl unmask virt${drv}d{,-ro,-admin}.socket
-  sudo systemctl enable virt${drv}d.service
-  sudo systemctl enable virt${drv}d{,-ro,-admin}.socket
+  sudo systemctl mask virt${drv}d.service
+  sudo systemctl mask virt${drv}d{,-ro,-admin}.socket
+  sudo systemctl disable virt${drv}d.service
+  sudo systemctl disable virt${drv}d{,-ro,-admin}.socket
 done
 
 #enable logd lockd
 for drv in log lock 
 do
-  sudo systemctl unmask virt${drv}d.service
-  sudo systemctl unmask virt${drv}d{,-admin}.socket
-  sudo systemctl enable virt${drv}d.service
-  sudo systemctl enable virt${drv}d{,-admin}.socket
+  sudo systemctl mask virt${drv}d.service
+  sudo systemctl mask virt${drv}d{,-admin}.socket
+  sudo systemctl disable virt${drv}d.service
+  sudo systemctl disable virt${drv}d{,-admin}.socket
 done
 
 #start services
 for drv in qemu network nodedev nwfilter secret storage
 do
-  sudo systemctl restart virt${drv}d{,-ro,-admin}.socket
+  sudo systemctl stop virt${drv}d{,-ro,-admin}.socket
 done
 
 #start logd lockd 
 for drv in log lock
 do
-  sudo systemctl restart virt${drv}d{,-admin}.socket
+  sudo systemctl stop virt${drv}d{,-admin}.socket
 done
 
-sudo systemctl unmask postgresql
-sudo systemctl enable postgresql
-sudo systemctl restart postgresql
+sudo systemctl mask postgresql
+sudo systemctl disable postgresql
+sudo systemctl stop postgresql
 
 #check service
 sudo systemctl is-active virtqemud.socket
